73c781878dcb00869fa3dd24cf02be9d53bf05ff
Nick97382000 <guerranicholas9738@gmail.com>
2026-06-15 22:52:39 +0200

    Update saving.py

--- saving.py
+++ saving.py
@@ -15,11 +15,13 @@ def save_json(input_pdf_path, output_dir, final_clustered_output):
         os.makedirs(output_dir)
         print(f"Created directory: {output_dir}")
     output_filename = os.path.basename(input_pdf_path).replace('.pdf', '_clustered_output.json')
-    output_path = os.path.join('Lettura_bilanci/output', output_filename)
+    output_path = os.path.join(output_dir, output_filename)
 
     # Preparazione dei dati
     serializable_output = make_serializable(final_clustered_output)
     
     with open(output_path, 'w', encoding='utf-8') as f:
         json.dump(serializable_output, f, ensure_ascii=False, indent=4)
+
+    print("saved " + input_pdf_path + " in " + output_dir)
     return 
